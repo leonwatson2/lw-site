83e4733e23bf074cf4a219d5b1d76231f5c556da
vlw0052 <leon@vlw2.com>
2018-06-09 13:08:15 -0500

    Added swipeable section for mobile in Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,7 +26,10 @@ export class Project extends Component {
   constructor(){
     super()
     this.state = {
-      shown: null
+      shown: null,
+      swipeLeftDelta: null,
+      swipeRightDelta: null,
+      swiping: false
     }
   }
   static propTypes = {
@@ -35,22 +38,48 @@ export class Project extends Component {
   static defaultProps = {
     project: { name: "", technologies: [ { name:"css3" } ], description:"lorem" }
   }
+  swipingLeft = (e, absX) => {
+    this.setState({ swipeLeftDelta:`${document.documentElement.clientWidth - absX}px`, swiping: true })
+  }
+  swipingRight = (e, absX) => {
+    this.setState({ swipeRightDelta:`${absX}px`, swiping: true })
+  }
+  swipedLeft = (e, abs)=>{ 
+    this.setState({ shown:true, swipeLeftDelta:null, swipeRightDelta:null, swiping: false }) 
+  }
+  swipedRight = (e, abs)=>{ 
+    this.setState({ shown:false, swipeLeftDelta:null, swipeRightDelta:null, swiping: false }) 
+  }
   render() {
     const { name, technologies, description } = this.props.project
-    const { shown } = this.state
+    const { shown, swipeLeftDelta, swipeRightDelta, swiping } = this.state
+    const classNames = `
+                  ${ shown ? "project__technologies--shown": "project__technologies"} 
+                  ${ swiping ? "swiping" : "" }
+                  `
+    let leftStyle = ""
+    if(swipeLeftDelta && !shown){
+      leftStyle = swipeLeftDelta
+    }else if(swipeRightDelta && shown){
+      leftStyle = swipeRightDelta
+    }
+                
     return (
-      <Swipeable onSwipedLeft = {(e, abs)=>{ this.setState({shown:true}) }}>
-        <div className="project">
-          <h3>{name}</h3>
-          <div className="project__info">{description}{shown}</div>
-          <div className={`${ shown ? "project__technologies--shown": "project__technologies"}`}>
-            {        
-              technologies.map(t => ( 
-                  <Technology  key={t.name} name={ t.name } />
-                )
+      <Swipeable onSwipedLeft = { this.swipedLeft }
+                  onSwipedRight = { this.swipedRight } 
+                  onSwipingLeft = { this.swipingLeft }
+                  onSwipingRight = { this.swipingRight }
+                  preventDefaultTouchmoveEvent = {true}
+                  className="project">
+        <h3>{name}</h3>
+        <div className="project__info">{description}</div>
+        <div style = { { 'left': leftStyle } } className={ classNames }>
+          {        
+            technologies.map(t => ( 
+                <Technology  key={t.name} name={ t.name } />
               )
-            }
-          </div>
+            )
+          }
         </div>
       </Swipeable>
     )
