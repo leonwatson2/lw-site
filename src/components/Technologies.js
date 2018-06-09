e188c2a8e59f66a70164e7e005b8629b9b085a11
vlw0052 <leon@vlw2.com>
2018-06-09 13:08:39 -0500

    Removed custom swipe code

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -18,9 +18,7 @@ export default class Technologies extends Component {
     const { technologies, classNames } = this.props
     return (
       <div className={["technologies", ...classNames ]}>
-      <h3 className="technologies__text">Things I Know
-          
-        </h3>
+      <h3 className="technologies__text">Things I Know</h3>
         <div className="technologies__list">
           {
             technologies.map( t => <Technology key={t.name} name = {t.name} />)
@@ -31,11 +29,7 @@ export default class Technologies extends Component {
     )
   }
 }
-function throttle(amount, fn){
-  setTimeout(() => {
-    fn()
-  }, amount);
-}
+
 export class Technology extends Component{
   static propTypes = {
     name: PropTypes.string.isRequired,
@@ -48,45 +42,20 @@ export class Technology extends Component{
   constructor(){
     super()
     this.state = {
-      active: false,
-      x: 0,
-      y: 0
+      active: false
     }
-    this.startX = null
-    this.startY = null
   }
   setActive = (bool, e)=>{
-    throttle(50, ()=> { e.preventDefault(); this.setState({ active:bool }) } )
-    if(bool){
-      this.startX = e.changedTouches[0].pageX
-      this.startY = e.changedTouches[0].pageY
-    }else{
-      this.startX = null
-      this.startY = null
-    }
+    this.setState({ active:bool });
   }
-  moveInfo = (e)=>{
-    if(this.state.active && this.props.move) {
-      e.persist()
-      e.preventDefault()
-      const { pageX, pageY } = e.changedTouches[0]
-      console.log(e.changedTouches[0])
-      const [xDist, yDist] = [ pageX - this.startX, pageY - this.startY ] 
-      const x = xDist > 30 ? 30 : xDist < -30 ? -30 : xDist
-      const y = yDist > 30 ? 30 : yDist < -30 ? -30 : yDist
-      this.setState({ x, y })
-    }  
-  }
+ 
   render(){
-    const { active, x, y } = this.state
+    const { active } = this.state
     const { name } = this.props
-    const transformStyle = { transform:`translate3d(${x}px, ${y}px, 0) scale(1.5)`}
     return <div 
                 onTouchStart={ (e) => this.setActive(true, e) } 
                 onTouchEnd={ (e) => this.setActive(false, e) }
-                onTouchMove={ this.moveInfo }
                 className={ `${active ? 'active':''} technologies__list--item` }
-                style={ active ? transformStyle: {} }
                 >
                 <div className="technologies__list--item-overlay">
                   {name}
